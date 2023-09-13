50efe9ae9ee391b2a6cfe1949a31e77c82673ad5
djohnson876 <djohnson876@lsr7sd.org>
2023-09-13 12:48:28 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("[welcome message]");
+
+    // int userInput = 0;
+    // while (true) {
+    //   // print context for current decision
+
+    //   // print list of options for current decision
+      
+    //   // read user input
+      
+    //   // update decision state for next iteration
+
+    //   if (userInput == 1) {
+    //     System.out.println("[exit message]");
+    //     break;
+    //   }
+    // }
+  }
+}
