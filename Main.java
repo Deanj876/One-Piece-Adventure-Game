5c5cf869326a23e4a39a697666234e3754cb9b06
djohnson876 <djohnson876@lsr7sd.org>
2023-09-13 13:04:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,43 @@
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-//Message Time Delay
-public static void timeDelay(){
-    try {
-        TimeUnit.SECONDS.sleep(2);
-      }
-    catch (InterruptedException ie) {
-        Thread.currentThread().interrupt();
-    }  
-}
-
 
 
 class Main {
-  public static void main(String[] args) {
+    //Message Time Delay
+    public static void timeDelay() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+          }
+        catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }  
+    }
+    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String playerName = "\"...\"";
-    System.out.println(playerName + ":Hello Grand Line!!!");
-    // int userInput = 0;
-    // while (true) {
-    //   // print context for current decision
+    System.out.println(playerName + ": Hello Grand Line!!!");
+    //Intro ^^
+    timeDelay();
+    System.out.println("\nA Mysterious Voice is heard in a Small hut");
+    System.out.println("\n\nPlayer Name :");
+    playerName = sc.nextLine();
+    System.out.println("\n\nShip Name :");
+    String ShipName = sc.nextLine();
 
-    //   // print list of options for current decision
-      
-    //   // read user input
-      
-    //   // update decision state for next iteration
-
-    //   if (userInput == 1) {
-    //     System.out.println("[exit message]");
-    //     break;
-    //   }
-    // }
+    //Start to the game
+    System.out.println(playerName + ": Time to start my adventure to become a great pirate!");
+    int gameLoop = 0;
+    while (true) {
+        timeDelay();
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
+        System.out.println("[exit message]");
+        gameLoop += 1;
+      if (gameLoop != 0) {
+        System.out.println("[exit message]");
+        break;
+      }
+    }
   }
 }
