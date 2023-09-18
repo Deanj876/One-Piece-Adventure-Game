4f0d47a7a313c82e0040f1c5f5e36ce23903c7df
djohnson876 <djohnson876@lsr7sd.org>
2023-09-18 13:23:20 -0500

    Update shop.java

--- shop.java
+++ shop.java
@@ -3,5 +3,7 @@ public class shop{
     public void playerShop(){
         System.out.print("\033[H\033[2J");  
         System.out.flush();
+        System.out.println("Shop Keeper: Hello, If you looking for goods to be a great Marine....or Pirate then you have come to the right place.\nHere is my Current Stock");
+    
     }
 }
