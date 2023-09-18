d77e6544d1daf13acf90674453389f67ad14e7fd
djohnson876 <djohnson876@lsr7sd.org>
2023-09-18 13:28:57 -0500

    Update shop.java

--- shop.java
+++ shop.java
@@ -4,6 +4,6 @@ public class shop{
         System.out.print("\033[H\033[2J");  
         System.out.flush();
         System.out.println("Shop Keeper: Hello, If you looking for goods to be a great Marine....or Pirate then you have come to the right place.\nHere is my Current Stock");
-    
+        System.out.println("\n\n----{Shop Stock}----\n\n1.Sword [Out of Stock]\n2.Flintlock [Out of Stock]\n3.Mysterious Fruit [1,200]");
     }
 }
