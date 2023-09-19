c6cb4d4d10529c0229ea128490bc3e7ce6d8ae50
djohnson876 <djohnson876@lsr7sd.org>
2023-09-19 13:34:42 -0500

    Update mapAndCombat.java

--- mapAndCombat.java
+++ mapAndCombat.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class mapAndCombat{
     Scanner sc = new Scanner(System.in);
     public void map(){
-        System.out.println("Your current log post is pointing to a small island known as \"Bandit Island\".");
+        while(true){
+            System.out.print("\033[H\033[2J");  
+            System.out.flush();
+            System.out.println("----Your current log post is pointing to a small island known as \"Bandit Island\".----");
+        }
     }
     public void Combat(){
     }
