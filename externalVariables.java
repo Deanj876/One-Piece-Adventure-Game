a7ee1cdc6c5366f59b89b11929283a1dcc6b682a
djohnson876 <djohnson876@lsr7sd.org>
2023-09-20 13:51:25 -0500

    Combat work up

--- externalVariables.java
+++ externalVariables.java
@@ -1,7 +1,8 @@
 class externalVariables{
-  int dayCounter = 1;
-  int belli = 500;
-  int devilFruitCounter = 0;
-  int logPostLevel = 0;
-  public String playerName = "\"...\"";
+  public static int dayCounter = 1;
+  public static int belli = 500;
+  public static int devilFruitCounter = 0;
+  public static int logPostLevel = 0;
+  public static String playerName = "\"...\"";
 }
+//Public static makes it on the same level as a class variable
