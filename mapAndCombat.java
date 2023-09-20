40c0e6d172eb5425ee6d409f15e9bed35983ebc9
Dean Johnson <143126442+Deanj876@users.noreply.github.com>
2023-09-20 10:25:04 -0500

    mapAndCombat.java

--- mapAndCombat.java
+++ mapAndCombat.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 public class mapAndCombat{
     Scanner sc = new Scanner(System.in);
+    public void combat(){
+      islandMethods im = new islandMethods();
+      im.banditIsland();
+    }
     public void map(){
+      Main m = new Main();
         while(true){
             System.out.print("\033[H\033[2J");  
             System.out.flush();
             System.out.println("----Your current log post is pointing to a small island known as \"Bandit Island\".----");
+            System.out.println("\nDo you wish to go to the Bandit Island? (Y/N):");
+            String islandChoice = sc.nextLine();
+            if(islandChoice.equals("y")){
+              System.out.println("\n----HEADING TO BANDIT ISLAND----");
+              m.timeDelay();
+              combat();
+            }
         }
-    }
-    public void Combat(){
     }
 }
