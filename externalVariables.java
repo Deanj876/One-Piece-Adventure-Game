32e5f5e079fd0baba2d51920925499f0db050a11
djohnson876 <djohnson876@lsr7sd.org>
2023-09-25 12:20:12 -0500

    Update externalVariables.java

--- externalVariables.java
+++ externalVariables.java
@@ -4,5 +4,6 @@ class externalVariables{
   public static int devilFruitCounter = 0;
   public static int logPostLevel = 0;
   public static String playerName = "\"...\"";
+  
 }
 //Public static makes it on the same level as a class variable
