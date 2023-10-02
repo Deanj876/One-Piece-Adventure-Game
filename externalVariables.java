abb84828896b9d44bd59d7dffaf2fd23dab75055
djohnson876 <djohnson876@lsr7sd.org>
2023-10-02 13:18:23 -0500

    Getting former progress back.

--- externalVariables.java
+++ externalVariables.java
@@ -1,24 +1,7 @@
-// class externalVariables extends Main{
-//   public int dayCounter = 1;
-//   public int belli = 500;
-//   public String playerName = "Hen";
+class externalVariables extends Main{
+  public int dayCounter = 1;
+  public int belli = 500;
+  public String playerName = "Hen";
   
-// }
-//Public static makes it on the same level as a class variable
-class externalVariables {
-    private playerStats playerStats;
-    private String playerName;
-    public int dayCounter;
-    public int belli;
-
-    public externalVariables(playerStats playerStats) {
-        this.playerStats = playerStats;
-        this.playerName = "Hen";
-        this.dayCounter = 1;
-        this.belli = 500;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
 }
+//Public static makes it on the same level as a class variable
